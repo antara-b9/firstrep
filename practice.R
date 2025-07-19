6874f3ef5577c09f4ccb9160cdd86ab498d29567
antara-b9 <antarabasak09@gmail.com>
2025-07-19 13:19:04 +0530

    Add files via upload

---
+++ practice.R
@@ -0,0 +1,19 @@
+v1 <- c("House in Hollywood", "House in London", "House in Japan")
+
+v2 <- c(2000000, 5000000, 29580000)
+
+v3 <- c("US Dollars", "Pounds", "Yen")
+real<-data.frame(j=v1,u=v2,n=v3)
+real
+data<-c(1,2,3,4,5,6,8,9,,)
+DATA1<-c(1,2,3)
+data2<-c(8,9,5)
+rbind(DATA1,data2)
+which(data=6)
+v1[2]
+data
+f<-c(data,5,8,data)
+data
+f
+rep(data(1,2,3,4,5,6,8,9),times=7)
+max(data,na.rm=TRUE)
